e2937af87c60035f6ebeeb7f733212b5f448eab1
HyeongChank <jsk381988@gmail.com>
2023-03-21 18:01:51 +0900

    update

--- JSPMission/src/main/webapp/mission1/el2.jsp
+++ JSPMission/src/main/webapp/mission1/el2.jsp
@@ -1,6 +1,7 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
-<%@ page import="eltest.eltest"%>  
+
+<%@ taglib prefix = "c" uri = "http://java.sun.com/jsp/jstl/core" %>
 <!DOCTYPE html>
 <html>
 <head>
@@ -8,12 +9,22 @@
 <title>Insert title here</title>
 </head>
 
-
-
 <body>
 <!-- request 저장되어 있는 값 호출 -->
+	<h1>${ar1 }</h1>
+<!-- for문은 forEach -->
+<!-- el 을 " " 안에 넣어야 함 -->
+	<c:forEach var ="i" begin = "0" end = "${ty2 }" >
+		<p>${array[i]}</p>
+		
+	</c:forEach>
+<!-- hash 값은 requestScope로 받아와야함 -->
+	<c:forEach var="i" items="${requestScope.hm}" >
+		${i.value }
+	</c:forEach>
 	<h1>${ty }</h1>	
 	<h1>${ty2}</h1>
 	<h1>dkfjdkfj</h1>
+	
 </body>
 </html>
---
+++ springbootquickstart/demo/src/main/java/edu/pnu/controller/BoardController.java
@@ -0,0 +1,45 @@
+package edu.pnu.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import edu.pnu.domain.Person;
+import edu.pnu.service.BoardService;
+
+@RestController
+public class BoardController {
+	
+	@Autowired
+	BoardService boardservice = new BoardService();
+	
+	public BoardController() {
+		System.out.println("===> BoardController 생성");
+	}
+	@GetMapping("/hello")
+	public String hello(String name) {
+		return "hello : " + name;
+	}
+	@PostMapping("/hello")
+	public String hello2(String name) {
+
+		return "hellopost : " + name;
+	}
+	@GetMapping("/getPerson")
+	public Person getPerson() {
+		return new Person("홍길동", 2000, "dkf", "선물");
+	}
+//	@GetMapping("/getPersons")
+//	public List<Person> getPersons(){
+//		List<Person> list = new ArrayList<>();
+//		list.add(new Person("홍길동", 2000, "dkf", "선물"));
+//		list.add(new Person("홍길동", 2000, "dkf", "선물"));
+//		return list;
+//	}
+
+}
